3f7d2e26480124e195e8f9bce3510ae19d393d4a
andrewwchong <andrewwchong@gmail.com>
2023-12-11 20:21:51 -0500

    struct inconsistency fixed

--- src/ece642rtle/monitors/achong2_face_monitor.cpp
+++ src/ece642rtle/monitors/achong2_face_monitor.cpp
@@ -2,7 +2,7 @@
  * Code by Milda Zizyte
  *
  * This monitor checks that the invariant "for any call to bumped(x1,y1,x2,y2), 
- * the turtle shall be facing the wall segment with endpoints (x1,y1) and (x2,y2).
+ * the turtle shall be facing the wall segment with endpoints_(x1,y1) and (x2,y2).
  * As a hint, note that this means there is only one valid wall segment that the turtle can call bumped(...) 
  * for any given position and orientation. You might consider computing this valid wall segment and comparing 
  * it to the arguments of bumped(...).
@@ -16,8 +16,10 @@ static Orientation last_orientation;
 static bool moved = false;
 static int X = -1;
 static int Y = -1;
-Point Point1{};
-Point Point2{};
+static int Point1_x = -1;
+static int Point1_y = -1;
+static int Point2_x = -1;
+static int Point2_y = -1;
 
 
 
@@ -46,7 +48,7 @@ void poseInterrupt(ros::Time t, int x, int y, Orientation o) {
 
 /*
  * Empty interrupt handlers beyond this point
- */
+_*/
 
 void tickInterrupt(ros::Time t) {
 }
@@ -61,31 +63,31 @@ void bumpInterrupt(ros::Time t, int x1, int y1, int x2, int y2, bool bumped) {
     //This is the wall segment that it should be facing
     switch(last_orientation){
             case NORTH:{
-                Point1.x = X;
-                Point1.y = Y;                
-                Point2.x = X+1;
-                Point2.y = Y;
+                Point1_x = X;
+                Point1_y = Y;                
+                Point2_x = X+1;
+                Point2_y = Y;
                 break;
             }
             case WEST:{
-                Point1.x = X;
-                Point1.y = Y;                
-                Point2.x = X;
-                Point2.y = Y+1;
+                Point1_x = X;
+                Point1_y = Y;                
+                Point2_x = X;
+                Point2_y = Y+1;
                 break;
             }
             case SOUTH:{
-                Point1.x = X;
-                Point1.y = Y+1;                
-                Point2.x = X+1;
-                Point2.y = Y+1;
+                Point1_x = X;
+                Point1_y = Y+1;                
+                Point2_x = X+1;
+                Point2_y = Y+1;
                 break;
             }
             case EAST:{
-                Point1.x = X+1;
-                Point1.y = Y;                
-                Point2.x = X+1;
-                Point2.y = Y+1;
+                Point1_x = X+1;
+                Point1_y = Y;                
+                Point2_x = X+1;
+                Point2_y = Y+1;
                 break;
             }
           default:{
@@ -94,10 +96,10 @@ void bumpInterrupt(ros::Time t, int x1, int y1, int x2, int y2, bool bumped) {
         }
 
   //If the x1,y1,x2,y2 don't match th expected value(checks for reversed as well)
-  if(!(((x1==Point1.x && y1 == Point1.y) && (x2==Point2.x && y2 == Point2.y)) ||
-         ((x1==Point2.x && y1 == Point2.y) && (x2==Point1.x && y2 == Point1.y)))){
+  if(!(((x1==Point1_x && y1 == Point1_y) && (x2==Point2_x && y2 == Point2_y)) ||
+         ((x1==Point2_x && y1 == Point2_y) && (x2==Point1_x && y2 == Point1_y)))){
     ROS_WARN("VIOLATION: Bump called on incorrect location: Start:(%d,%d) End:(%d,%d)",x1,y1,x2,y2);
-    ROS_WARN("VIOLATION: Correct location: Start:(%d,%d) End:(%d,%d)",Point1.x,Point1.y,Point2.x,Point2.y);
+    ROS_WARN("VIOLATION: Correct location: Start:(%d,%d) End:(%d,%d)",Point1_x,Point1_y,Point2_x,Point2_y);
 
   }
 }
